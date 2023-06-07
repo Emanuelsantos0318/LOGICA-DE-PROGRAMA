66f03a95d978ac4ce460baa61e488e5dbd2ec93b
Emanuel <128701097+Emanuelsantos0318@users.noreply.github.com>
2023-06-07 11:29:26 -0300

    Update funcao.c

--- C/funcao.c
+++ C/funcao.c
@@ -1,37 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Crie uma função em linguagem C que receba 2 números e retorne o maior
-//e o menor valor.
+// Crie uma função em linguagem C que receba 2 números e retorne o maior
+// e o menor valor.
 
-void maiormenor (float maior, float menor, float n1, float n2){
-    if(n1 > n2){
-maior= n1;
-menor= n2;
-} else{
-maior= n2;
-menor= n1;
+void maiores(int num1, int num2)
+{
+    int maior;
+    if (num1 > num2)
+    {
+        maior = num1;
+    }
+    else
+    {
+        maior = num2;
+    }
+    printf("O maior numero e: %i \n", maior);
+
+    return maior;
+}
+void menores(int num1, int num2)
+{
+    int menor;
+    if (num1 > num2)
+    {
+
+        menor = num2;
+    }
+    else
+    {
+
+        menor = num1;
+    }
+
+    printf("O menor numero e: %i \n", menor);
+
+    return menor;
 }
 
+void cabecalho()
+{
+    printf("______________________________________\n");
+    printf("\t FUNCAO MAIOR & MENOR\n");
+    printf("______________________________________\n");
 }
 
-int main () {
+int main()
+{ // COMANDO PRINCIPAL OU SE PREFERIR O COMANDO QUE É MOSTRADO.
 
-int n1, n2, maior, menor;
+    int n1, n2;
 
+    cabecalho();
 
+    printf("Digite um numero: \n");
+    scanf("%i", &n1);
 
-printf("Digite um numero: \n");
-scanf("%i", &n1);
+    printf("Digite outro numero: \n");
+    scanf("%i", &n2);
 
-printf("Digite outro numero: \n");
-scanf("%i", &n2);
+    maiores(n1, n2);
+    menores(n1, n2);
 
-printf("o maior numero e: %i \n", maior);
-printf("O menor numero e: %i \n", menor);
-
-return 0;
-
-
-
+    return 0;
 }
